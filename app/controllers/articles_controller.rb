e7645232d79b99d6afe35fddbbe38df4b9b0dcd4
Charles Grant Nichols <grant@inkdots.com>
2021-06-15 11:22:49 -0500

    commit tree 11d1a418eacb9a0d553819ed508d0189f5e33390

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,19 +6,16 @@ class ArticlesController < ApplicationController
     render json: articles, each_serializer: ArticleListSerializer
   end
 
-  # def show
-  #   article = Article.find(params[:id])
-  #   render json: article
-  # end
-
   def show
-    session[:pageviews_remaining] ||= 3
+    
+    # byebug
+    session[:pageviews_remaining]||= 3
     session[:pageviews_remaining] -= 1
     if session[:pageviews_remaining] > 0
       article = Article.find(params[:id])
-      render json: article 
+      render json: article
     else
-      render json: {error: "Maximum pageview limit reached"}, status: :unauthorized 
+      render json: {error: "Maximum pageview limit reached"}, status: :unauthorized
     end
   end
 
